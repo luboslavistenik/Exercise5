27e7b46e8c5f2ae060e0cd66b52d97e8a9ea8a97
luboslavistenik <luboslav.istenik@gmail.com>
2020-10-19 22:12:06 +0200

    Formating

--- src/com/company/equalSlices.java
+++ src/com/company/equalSlices.java
@@ -3,12 +3,12 @@ package com.company;
 public class equalSlices {
 
     public static void main(String[] args) {
-	double total = 10;
-	double recipients = 5;
-    double each = 2;
-    if(total / each >= recipients)
-        System.out.println(true);
-    else
-        System.out.println(false);
+	    double total = 10;
+	    double recipients = 5;
+        double each = 2;
+        if(total / each >= recipients)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
